8e1b974c2e2a607178dca1b71b726462e6b28107
shz295 <shz295@nate.com>
2023-08-01 17:13:47 +0900

    Some changes

--- in_sample_test.py
+++ in_sample_test.py
@@ -4,84 +4,96 @@
 ## outputs: signal quality metrics (sharpe ratio, pnl, turnover, fitness, drawdown)
 
 import pandas as pd
-import pandas_datareader.data as pdr
+import requests
 from signals import five_day_reversion
 from math import sqrt
 
-# get S&P 500 tickers list from wikipedia
-table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
-sp500 = table[0]["Symbol"].values.tolist()
-universes = {"US10": ["AAPL", "MSFT", "GOOG", "AMZN", "BRK-A", "TSLA", "UNH", "XOM", "JNJ", "V"], "SP500": sp500}
+US10 = ["AAPL", "MSFT", "GOOG", "AMZN", "BRK-A", "TSLA", "UNH", "XOM", "JNJ", "V"]
 
 config = {
-	'universe': universes["US10"],
-	'start': '2017-01-01',
-	'end': '2020-12-31',
+	'universe': US10,
+	'start': '2019-01-01',
+	'end': '2022-12-31',
 	'signal': five_day_reversion,
 	'neutralization': 'all',
-	'investment': 50000
+	'investment': 50000,
+	'API_KEY': '3780b5d18a15253a9e1d33d483ad4dccd49ae20e',
 }
 
-def in_sample_test():
+def get_data(universe, start, end):
 	## import data from universe
-	df = pdr.DataReader(config['universe'], 'yahoo', config['start'], config['end'])
-	df = df[['Close', 'Volume']] # drop unneeded data
-	df = df.stack() # let tickers be a column instead of row
-	df.index.names = ['date', 'ticker']
-	df.columns = ['close', 'volume']
-	df, signal_name = config['signal'](df)
+	df = pd.DataFrame()
+	for ticker in universe:
+		url = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices"
+		response = requests.get(url, params={"token": config['API_KEY'], 'startDate': start, 'endDate': end})
+		data = response.json()
+		data = pd.DataFrame(data)
+		data = data[['date', 'close', 'volume']]
+		data['ticker'] = ticker
+		df = pd.concat([df, data], axis=0)
+	df['date'] = pd.to_datetime(df['date']).dt.date
+	df.set_index(['date', 'ticker'], inplace=True)
+	return df
 
+def gen_positions(df, investment):
 	## generate position vectors for each day
+	df['signal'] = config['signal'](df)
 	if config['neutralization'] == 'all':
-		short_groups = df[df[signal_name] < 0].groupby(level=0)
-		short_scale = -config['investment'] / short_groups.sum()
-		df[signal_name + "_position"] = short_scale[signal_name] * df[signal_name]
+		short_groups = df[df['signal'] < 0].groupby(level=0)
+		short_scale = -investment / short_groups.sum()
+		df["position"] = short_scale['signal'] * df['signal']
 
-		long_groups = df[df[signal_name] >=0].groupby(level=0)
-		long_scale = config['investment'] / long_groups.sum()
-		df.loc[df[signal_name] >=0, signal_name + "_position"] = long_scale[signal_name] * df[signal_name]
+		long_groups = df[df['signal'] >=0].groupby(level=0)
+		long_scale = investment / long_groups.sum()
+		df.loc[df['signal'] >=0, "position"] = long_scale['signal'] * df['signal']
+	return df
 
+def gen_pnl(df):
 	## generate pnl vectors from position vectors
 	df['pnl'] = df.groupby(['ticker'])['close'].pct_change()
-	df[signal_name + "_pnl"] = df[signal_name + '_position'] * df['pnl']
+	df['pnl'] = df['position'] * df['pnl']
+	return df
 
+def gen_metrics(df, investment):
 	## generate signal metrics from pnl vectors
-	df['trading'] = df[signal_name+'_position'].groupby(['ticker']).diff(periods=1).abs()
+	df['trading'] = df['position'].groupby(['ticker']).diff(periods=1).abs()
 	df = df.dropna()
 	df = df.reset_index(level=1)
 	df.index = pd.to_datetime(df.index)
-	metrics = df[signal_name + '_pnl'].groupby(pd.Grouper(freq='Y')).sum().to_frame()
+	metrics = df['pnl'].groupby(pd.Grouper(freq='Y')).sum().to_frame()
 	metrics.columns = ['total_pnl']
-	metrics['pnl'] = 100 * metrics['total_pnl'] / (config['investment'] * 2)
-	metrics['ir'] = metrics['pnl'] / df[signal_name + '_pnl'].groupby(level=0).sum().groupby(pd.Grouper(freq='Y')).std()
-	metrics['sharpe'] = metrics['ir']*sqrt(252)
-	metrics['turnover'] = 100 * df['trading'].groupby(pd.Grouper(freq='Y')).sum() / (config['investment'] * 252 * 2)
-	metrics['margin'] = 100 * metrics['total_pnl'] / df['trading'].groupby(pd.Grouper(freq='Y')).sum()
-	metrics['fitness'] = metrics['sharpe'] * (metrics['pnl'].abs() / metrics['turnover']).apply(sqrt)
+	metrics['pnl'] = 100 * metrics['total_pnl'] / (investment * 2) # profit and loss
+	metrics['ir'] = metrics['pnl'] / df['pnl'].groupby(level=0).sum().groupby(pd.Grouper(freq='Y')).std() # information ratio
+	metrics['sharpe'] = metrics['ir']*sqrt(252) # sharpe ratio
+	metrics['turnover'] = 100 * df['trading'].groupby(pd.Grouper(freq='Y')).sum() / (investment * 252 * 2) # turnover
+	metrics['margin'] = 100 * metrics['total_pnl'] / df['trading'].groupby(pd.Grouper(freq='Y')).sum() # margin
+	metrics['fitness'] = metrics['sharpe'] * (metrics['pnl'].abs() / metrics['turnover']).apply(sqrt) # fitness ratio
+	return df, metrics
 
+def gen_drawdown(df, investment):
 	drawdowns = []
-	for year, j in df[signal_name + '_pnl'].groupby(pd.Grouper(freq='Y')):
+	for year, j in df['pnl'].groupby(pd.Grouper(freq='Y')):
 		max_drawdown = 0
 		for i in range(len(j.groupby(level=0))):
 			val = j.groupby(level=0).sum().iloc[i]
 			if val<0:
 				curr=i
 				drawdown = val
+				if curr == len(j.groupby(level=0))-1: break
 				while(j.groupby(level=0).sum().iloc[curr+1] < 0):
 					drawdown += j.groupby(level=0).sum().iloc[curr+1]
 					curr += 1
-				if drawdown < max_drawdown: 
-					max_drawdown = drawdown
+					if curr == len(j.groupby(level=0))-1: break
+				max_drawdown = min(max_drawdown, drawdown)
 		drawdowns.append(max_drawdown)
 	
-	metrics['drawdown'] = [- 100 * x / config['investment'] for x in drawdowns]
-
-	print(metrics)
-	metrics.to_csv("D:\\proj\\trading_system\\metrics.csv")
-
-
-	#print(df.tail())
-	#df.to_csv("D:\\proj\\trading_system\\dat_df.csv")
+	return [- 100 * x / investment for x in drawdowns]
 
 if __name__ == "__main__":
-	in_sample_test()
+	df = get_data(config['universe'], config['start'], config['end'])
+	df = gen_positions(df, config['investment'])
+	df = gen_pnl(df)
+	df, metrics = gen_metrics(df, config['investment'])
+	metrics['drawdowns'] = gen_drawdown(df, config['investment'])
+	print(metrics)
+	metrics.to_csv(f"D:\\proj\\trading_system\\{config['signal'].__name__}.csv")
--- signals.py
+++ signals.py
@@ -1,4 +1,4 @@
 def five_day_reversion(df):
 	df['5dr'] = df.groupby(['ticker'])['close'].diff(periods=5) / df['close']
 	df['5dr'] = df['5dr'] * (1 / df.abs().groupby(level=0)['5dr'].max())
-	return df, "5dr"
+	return df['5dr']
